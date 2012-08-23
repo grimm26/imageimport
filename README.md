4ffc300507362fb9f7cbf9ad05f0ff691747c3f4
Mark Keisler <grimm26@gmail.com>
2012-08-23 08:32:15 -0500

    Expand README, add TODO, initial watch_and_import.pl commit

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ imageimport
 ===========
 
 Watch a directory for images to be dumped into to rename and organize them.
+
+
+The Itch
+========
+I have used a script to take images off of my camera memory cards and organize them by putting
+them into directories named by the date they were taken and renaming the image to the timestamp
+it was taken.  For example: IMG1213.JPG becomes 2012_07_28/2012_07_28-12_07_30.jpg
+The problem with this became that to upload my pictures I needed to sit in front of a Linux
+box with an SD card reader.
+
+The Scratch
+===========
+I decided to put the work onto the server.  Using inotify, I can watch a directory for when
+pictures get added to it and then do my thing.  It no longer matters how the pictures get
+taken off of the camera as long as they get put into the "dump" directory for new pictures.
---
+++ watch_and_import.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+# This script will monitor a directory that images from your camera or wherever will be downloaded to.
+# As new images come in, the script will rename them based on EXIF timestamp data.
+
+# External requirements: jhead
+
+use Linux::Inotify2;
+use Getopt::Long;
+
+use warnings;
+use strict;
+
+my $debug = 0;
+GetOptions ("debug" => \$debug );
+
+# Where images will be dumped.
+use constant DUMP_DIR => '/raid/media/pictures/dumpdir/';
+use constant DEST_DIR => '/raid/media/pictures/';
+
+# create a new object
+my $inotify = new Linux::Inotify2
+    or die "unable to create new inotify object: $!";
+ 
+# watch for when a write filehandle is closed.
+$inotify->watch (DUMP_DIR, IN_CLOSE_WRITE, sub {
+    my $e = shift;
+    my $fullname = $e->fullname;
+    my $name = $e->name;
+    my @list = ();
+    print "$fullname ($name) was written.\n" if $debug
+    if ( $name =~ /\.jpg$/i ) {
+        sleep 1;
+        open JHEAD, "jhead $fullname|" or die "Could not jhead $fullname\n";
+        while (<JHEAD>) {
+            if (m!^Date/Time\s+:\s+(\d+):(\d+):(\d+)\s+(\d+):(\d+):(\d+)!) {
+                my $year  = $1;
+                my $month = $2;
+                my $day   = $3;
+                my $hour  = $4;
+                my $min   = $5;
+                my $sec   = $6;
+                my $dest  = DEST_DIR. "${year}_${month}_${day}";
+                if (-r "$dest/${year}_${month}_${day}-${hour}_${min}_${sec}.jpg") {
+                    print "Skipping duplicate $dest/${year}_${month}_${day}-${hour}_${min}_${sec}.jpg\n" 
+                        if $debug;
+                    unlink $fullname;
+                    last;
+                }
+                if (-r "$dest/${year}_${month}_${day}-${hour}-${min}_${sec}.jpg") {
+
+                    print "Skipping duplicate $dest/${year}_${month}_${day}-${hour}-${min}_${sec}.jpg\n"
+                        if $debug;
+                    unlink $fullname;
+                    last;
+                }
+                push @list, $dest;
+                system "mkdir $dest" unless -d "$dest";
+                rename($fullname, "$dest/$name") or warn "Could not move $name: $!\n";
+                system "jhead  -n%Y_%m_%d-%H_%M_%S $dest/$name";
+                last;
+            }
+        }
+        close JHEAD;
+        foreach (@list) {
+            system "chmod -R 775 $_";
+        }
+    }
+    });
+
+# keep polling :)
+1 while $inotify->poll;
+
