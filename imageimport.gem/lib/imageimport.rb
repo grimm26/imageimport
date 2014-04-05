e1267913eb2e83a0c9d5dac8c9f5b5491c5bf84c
Mark Keisler <grimm26@gmail.com>
2014-04-04 21:01:15 -0500

    create accessors for @logger and @destination.  No need to make notifier an instance variable.

--- imageimport.gem/lib/imageimport.rb
+++ imageimport.gem/lib/imageimport.rb
@@ -9,6 +9,7 @@ require 'date'
 module ImageImport
   class Watch
     include EXIFR
+    attr_accessor :logger, :destintation
 
     def initialize(watch: nil,destination: nil, logger: Logger.new(STDERR))
       raise "Error accessing watchdir <<#{watch}>>" unless File.directory?(watch)
@@ -17,37 +18,36 @@ module ImageImport
       @logger = logger
       @destination = destination
       logger.info("Initializing ImageWatch on #{watch}.  Destination: #{destination}")
-      @notifier = INotify::Notifier.new
-      @notifier.watch(watch, :moved_to, :close_write) { |event| processInotifyEvent(event) }
-      @notifier.run
+      notifier = INotify::Notifier.new
+      notifier.watch(watch, :moved_to, :close_write) { |event| processInotifyEvent(event) }
+      notifier.run
     end
 
     private
     def processInotifyEvent(event)
       filepath = event.absolute_name
-      @logger.debug("Processing #{filepath}...")
+      logger.debug("Processing #{filepath}...")
       if FileMagic.open(:mime_type) { |fm| fm.file(filepath) } == 'image/jpeg'
-        @logger.info("Found new jpeg: #{filepath}")
+        logger.info("Found new jpeg: #{filepath}")
         dt = JPEG.new(filepath).date_time
         if dt == nil
-          @logger.info("No date info in #{filepath}. Skipping.")
+          logger.info("No date info in #{filepath}. Skipping.")
         else
-          ts = dt.strftime("%Y_%m_%d-%H_%M_%S")
-          dest_dir  = File.join(@destination,dt.strftime("%Y_%m_%d"))
+          dest_dir  = File.join(destination,dt.strftime("%Y_%m_%d"))
           dest_file = File.join(dest_dir,dt.strftime("%Y_%m_%d-%H_%M_%S") + ".jpg")
 
           if File.exist?(dest_file)
-              @logger.info("Skipping duplicate #{dest_file}")
+              logger.info("Skipping duplicate #{dest_file}")
             begin
               File.delete(filepath)
             rescue Exception => e
-              @logger.warn("Failed removal of #{filepath}: #{e.message}")
+              logger.warn("Failed removal of #{filepath}: #{e.message}")
             end
           else
             begin
-              @logger.info("Moving #{filepath} to #{dest_file}")
+              logger.info("Moving #{filepath} to #{dest_file}")
               unless Dir.exist?(dest_dir)
-                @logger.debug("Creating new destination directory #{dest_dir}")
+                logger.debug("Creating new destination directory #{dest_dir}")
                 Dir.mkdir(dest_dir)
                 File.chown(nil,444,dest_dir)
                 File.chmod(02775,dest_dir)
@@ -56,7 +56,7 @@ module ImageImport
               File.chmod(0664,dest_file)
               File.chown(nil,444,dest_file)
             rescue SystemCallError => e
-              @logger.error("Error processing #{dest_file}: #{e.message} #{e.backtrace[0]}")
+              logger.error("Error processing #{dest_file}: #{e.message} #{e.backtrace[0]}")
             end
           end
         end
