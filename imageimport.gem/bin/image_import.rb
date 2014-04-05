7e0227ff2b830419eed5fb3b15b3c51146859756
Mark Keisler <grimm26@gmail.com>
2014-04-04 21:56:57 -0500

    change options to an OpenStruct.  Catch and handle OptionParser exceptions.  Add pry and pry-nav as dev dependencies

--- imageimport.gem/bin/image_import.rb
+++ imageimport.gem/bin/image_import.rb
@@ -2,11 +2,11 @@
 #
 require 'imageimport'
 require 'optparse'
+require 'ostruct'
 require 'logger'
 
-
-
-loglevels = { 'DEBUG'   => Logger::DEBUG,
+loglevels = {
+              'DEBUG'   => Logger::DEBUG,
               'INFO'    => Logger::INFO,
               'WARN'    => Logger::WARN,
               'ERROR'   => Logger::ERROR,
@@ -14,14 +14,15 @@ loglevels = { 'DEBUG'   => Logger::DEBUG,
               'UNKNOWN' => Logger::UNKNOWN
             }
 
-
-options = {}
+options = OpenStruct.new
+options.watchdir    = 'junk'
+options.destination = nil
 optparse = OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
 
   opts.on("-w DIRECTORY","--watchdir", "What directory to watch for incoming jpegs.") do |dir|
     if Dir.exists?(dir)
-      options[:watchdir] = dir
+      options.watchdir = dir
     else
       opts.abort "Supplied watchdir, #{dir}, is not a valid directory."
     end
@@ -29,35 +30,39 @@ optparse = OptionParser.new do |opts|
 
   opts.on("-d DIRECTORY","--destination", "The top of the directorty in which to place imported images.") do |dir|
     if Dir.exists?(dir)
-      options[:destination] = dir
+      options.destination = dir
     else
       opts.abort "Supplied destination, #{dir}, is not a valid directory."
     end
   end
 
-  options[:loglevel] = loglevels['INFO']
+  options.loglevel = loglevels[:INFO]
   opts.on("--loglevel LEVEL", loglevels,  "Logging level (DEBUG, INFO, WARN, ERROR, FATAL, UNKNOWN).") do |level|
-    options[:loglevel] = level
+    options.loglevel = level
   end
 
-  options[:logfile] = STDERR
+  options.logfile = STDERR
   opts.on("--logfile FILE", "File or filehandle to log to.") do |file|
-    options[:logfile] = file
+    options.logfile = file
   end
 end
 
-optparse.parse
-if options[:watchdir].nil? || options[:destination].nil?
+begin
+  optparse.parse!
+rescue Exception => e
+  optparse.abort "#{e.message}\n\n#{optparse.help}"
+end
+if options.watchdir.nil? || options.destination.nil?
   optparse.abort "Both --watchdir and --destination must be supplied.\n\n#{optparse.help}"
 end
 
 begin
-  log = Logger.new(options[:logfile],3,100 * 1024 * 1024)
-  log.level = options[:loglevel]
+  log = Logger.new(options.logfile,3,100 * 1024 * 1024)
+  log.level = options.loglevel
   log.datetime_format = '%Y-%m-%d %H:%M:%S'
 rescue SystemCallError => e
   STDERR.puts e.message
   STDERR.puts e.backtrace.inspect
 end
 
-ImageImport::Watch.new(watch: options[:watchdir], destination: options[:destination], logger: log)
+ImageImport::Watch.new(watch: options.watchdir, destination: options.destination, logger: log)
--- imageimport.gem/imageimport.gemspec
+++ imageimport.gem/imageimport.gemspec
@@ -22,6 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "pry"
+  spec.add_development_dependency "pry-nav"
   spec.add_dependency "rb-inotify"
   spec.add_dependency "ruby-filemagic"
   spec.add_dependency "exifr"
